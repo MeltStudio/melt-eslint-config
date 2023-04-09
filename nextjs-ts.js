a976795eb7ec90f4d6fc5e8fbedeabfcf1cbd7f9
piedrahitapablo <piedra@meltstudio.co>
2023-04-09 18:29:08 -0500

    fix: update nextjs-ts to avoid errors when using style tags

--- nextjs-ts.js
+++ nextjs-ts.js
@@ -2,10 +2,16 @@ module.exports = {
   extends: ['plugin:@next/next/recommended', require.resolve('./react-ts')],
   rules: {
     'jsx-a11y/alt-text': [
-      'warn',
+      'error',
       {
         elements: ['img'],
         img: ['Image'],
+      },
+    ],
+    'react/no-unknown-property': [
+      'error',
+      {
+        ignore: ['jsx', 'global'],
       },
     ],
   },
